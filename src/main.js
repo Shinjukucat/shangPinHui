991ef76bba144821b31a61fa46d19fdfa2a84c5a
Shinjuku-cat <1406690842@qq.com>
2022-08-02 16:40:37 +0800

    2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from '@/router/index'
 // 引入store
 import store from '@/store'
+// 引入element-ui
+import {Button, MessageBox} from 'element-ui'
 
 // 阻止显示当前生成模式的代码
 // Vue.config.productionTip = false
@@ -17,17 +19,37 @@ import Pagination from '@/components/Pagination'
 Vue.component(TabNav.name, TabNav)
 Vue.component(Carousel.name, Carousel)
 Vue.component(Pagination.name, Pagination)
+// 全局注册element-ui的组件，因为是按需引入，所以需要这一步
+Vue.component(Button.name, Button)
+// 另外一种注册element-ui组件的方法，挂载在原型上
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
 
 // 引入mock数据
 import '@/mock/mockServer'
 
 // 引入swiper样式
 import 'swiper/css/swiper.css'
+
+// 引入api文件夹里所有api接口函数
+import * as http from '@/api'
+
+// 引入图片懒加载的插件
+// import VueLazyload from 'vue-lazyload'
+// 引入图片懒加载的图片
+// import lazyloadingImg from '@/assets/lazyloading.gif'
+// 注册插件
+// Vue.use(VueLazyload, {
+//   loading: lazyloadingImg
+// })
+
 new Vue({
   render: h => h(App),
   // 配置全局总线$bus
   beforeCreate() {
     Vue.prototype.$bus = this
+    // 将这个api接口函数挂载到整个vue组件上，这个$http是组件随便取得名字，想怎么取怎么取，从这里开始P98使用直接调用api接口的方法，不再用vuex间接调用
+    Vue.prototype.$http = http
   },
   // 注册路由，因为key和value一致，所以可以省略value
   // 注册完路由后，路由组件和非路由组件身上都会具有$route和$router属性
